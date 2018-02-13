dfcb71a8f6a704e4a2990f0615c5f1b0635e1ec9
telosma <duylxbk57@gmail.com>
2018-02-13 16:43:50 +0700

    Reinstall npm, install moment.js

--- test/CountDown.spec.js
+++ test/CountDown.spec.js
@@ -1,28 +1,39 @@
 import { mount } from '@vue/test-utils'
+import Countdown from '../src/components/Countdown.vue'
+import moment from 'moment'
 import expect from 'expect'
-import CountDown from '../src/components/CountDown.vue'
 
 describe ('Countdown', () => {
-	beforEach (() => {
+    let wrapper
 
-	})
+    beforeEach (() => {
+        wrapper = mount(Countdown)
+    })
+
+    it ('renders a Countdown timer', () => {
+        wrapper.setProps({ util: moment().add(10, 'seconds') })
+
+        see('10 seconds')
+    })
+
+
+    // Helper Functions
+
+    let see = (text, selector) => {
+        let wrap = selector ? wrapper.find(selector) : wrapper
+
+        expect(wrap.html()).toContain(text)
+    }
+
+    let type = (text, selector) => {
+        let node = wrapper.find(selector)
+
+        node.element.value = text
+        node.trigger('input')
+    }
+
+    let click = selector => {
+        wrapper.find(selector).trigger('click')
+    }
+
 })
-
-	// Helper Functions
-
-	let see = (text, selector) => {
-		let wrap = selector ? wrapper.find(selector) : wrapper
-
-		expect(wrap.html()).toContain(text)
-	}
-
-	let type = (text, selector) => {
-		let node = wrapper.find(selector)
-
-		node.element.value = text
-		node.trigger('input')
-	}
-
-	let click = selector => {
-		wrapper.find(selector).trigger('click')
-	}
